adb5ac4242542a4d12c69d703943bc5677f342e1
bcalou <bastien.calou@gmail.com>
2020-03-15 19:37:04 +0100

    05 Variables d'environnement

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
-const port = 3000;
+
+dotenv.config();
 
 app.get('/', function(req, res) {
   res.send('Hello world');
 });
 
-app.listen(port, () => console.log(`Front app listening on port ${port}!`));
+app.listen(process.env.PORT, () =>
+  console.log(`Front app listening on port ${process.env.PORT}!`),
+);
